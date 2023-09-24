32ef638dda06056076797e4a2e20e2a227d8fafb
manish-giri <manish.giri.me@gmail.com>
2023-09-23 21:52:02 -0500

    Generating new deck

--- project-cards/deck.go
+++ project-cards/deck.go
@@ -5,6 +5,31 @@ import "fmt"
 // A new "type" that "extends" the behavior of a slice of strings
 type deck []string
 
+// Generate new deck of cards
+func newDeck() deck {
+	fmt.Println("Creating new deck...")
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+// A receiver function that allows all values of type deck to have access to printDeck()
+func (d deck) printDeck() {
+	fmt.Println("Printing deck...")
+	for _, card := range d {
+		fmt.Println(card)
+	}
+	fmt.Println("*********")
+}
+
 // A receiver function that allows all values of type deck to have access to print()
 func (d deck) print() {
 	fmt.Println("Calling print()...")
--- project-cards/main.go
+++ project-cards/main.go
@@ -1,16 +1,9 @@
 package main
 
 func main() {
-	// declare slice
-	cards := deck{newCard(), "Five of hearts", "Six of spades"}
-
-	// add one more card
-	cards = append(cards, "Two of clubs")
+	// generate deck of cards
+	cards := newDeck()
 
 	// use type deck's function to print
-	cards.print()
-}
-
-func newCard() string {
-	return "Ace of diamonds"
+	cards.printDeck()
 }
